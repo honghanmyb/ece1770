05970dc0d9e50b59f6b0c17f6f763055aaec1ada
Han Hong <hanhong@unknowns-MacBook-Pro.local>
2020-04-10 01:42:46 -0400

    change test code so it can run on geth console, add test result

--- geth-test.js
+++ geth-test.js
@@ -1,22 +1,23 @@
-const transactionNum = 1000
-let sender = eth.accounts[0]
-let receiver = eth.accounts[1]
-const transactionEndTime = []
+var transactionNum = 1000
+var sender = eth.accounts[0]
+var receiver = eth.accounts[1]
+var transactionEndTime = []
 miner.start()
 while(transactionEndTime.length <= transactionNum){
-    const accountArr = personal.listWallets
-    for(let i = 0; i < accountArr.length; i++){
+    var accountArr = personal.listWallets
+    for(var i = 0; i < accountArr.length; i++){
         if(accountArr[i].status === "Locked"){
-            personal.unlockAccount(accountArr[i].accounts[0].address, "123456")
+            personal.unlockAccount(accountArr[i].accounts[0].address, "123456", 0)
         }
     }
     if(eth.pendingTransactions.length == 0){
-        const time = Date.now()
+        var time = Date.now()
         transactionEndTime.push(time)
-        let temp = sender
+        var temp = sender
         sender = receiver
         receiver = temp
         eth.sendTransaction({from:sender, to:receiver, value: web3.toWei(1000, "ether")})
+        console.log(transactionEndTime.length)
     }
 }
 miner.stop()
